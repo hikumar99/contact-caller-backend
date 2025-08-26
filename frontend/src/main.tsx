00fddd2e1fe939b75bad83a839cbcbfd69266055
Kumar K <hikumarswamy@gmail.com>
2025-08-26 22:40:45 +0530

    Add files via upload

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ContactCallerApp from "./contact_caller_app";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ContactCallerApp />
+  </React.StrictMode>
+);
